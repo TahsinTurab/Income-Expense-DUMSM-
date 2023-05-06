b1ec27de96e76afbf74fe975bca9a9eae3fe1fd5
Tahsin <tahsinhasanturab@gmail.com>
2023-05-06 09:13:39 +0600

    data display doner list done

---
+++ DUMSM/DUMSM/DataGridViewOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace DUMSM
+{
+    internal static class DataGridViewOperation
+    {
+        private static string connectionString = SQLConnection.conReturn();
+        public static void DisplayData(DataGridView dataGrid, string tableName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = $"select * from {tableName}";
+                //SqlCommand command = new SqlCommand(CommandText, connection);
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                da.Fill(dt);
+
+                
+                dataGrid.DataSource = dt;
+                connection.Close();
+            }
+        }
+    }
+}
